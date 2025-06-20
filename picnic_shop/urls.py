e8584a7e995c4af0b97a1468f78a414db230a65f
AnnLiubavetskaya <20anita07l@gmail.com>
2025-06-20 18:07:22 +0300

    15 лаба 1 версия

--- picnic_shop/urls.py
+++ picnic_shop/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from .views import author
 from .views import shop
 from .views import main
+from . import views
 
 urlpatterns = [
     path('main/author.html', author, name='author'),
     path('main/shop.html',shop, name = 'shop' ),
     path('main/',main, name = 'main' ),
+    path('spec/', views.qualifications_list, name='qualifications_list'),
+    path('spec/<int:id>/', views.qualification_detail, name='qualification_detail'),
 ]
--- picnic_shop/views.py
+++ picnic_shop/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
+import json
+from django.http import Http404
+
+# Загрузка данных из JSON
+def load_qualifications():
+    with open('picnic_shop/dump.json', 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+# Список всех квалификаций
+def qualifications_list(request):
+    qualifications = load_qualifications()
+    return render(request, 'qualifications_list.html', {'qualifications': qualifications})
+
+# Детали одной квалификации
+def qualification_detail(request, id):
+    qualifications = load_qualifications()
+    qualification = next((q for q in qualifications if q['id'] == id), None)
+    
+    if not qualification:
+        raise Http404("Квалификация не найдена")
+    
+    return render(request, 'qualification_detail.html', {'qualification': qualification})
 
 def author(request):
     return render(request, 'author.html');
